3f23a6b75d4931366f1d61a9fec1da248826a99b
PrashantDesale2004 <prashantdesale2004@gmail.com>
2024-11-30 17:33:38 +0530

    spring security

---
+++ src/main/java/com/oauth/security/Controller.java
@@ -0,0 +1,12 @@
+package com.oauth.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String greet(){
+        return "hello";
+    }
+}
